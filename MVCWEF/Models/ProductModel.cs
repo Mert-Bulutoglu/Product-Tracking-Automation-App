29baebc63740c21e54fa255faab27bea4f5fb8c5
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2022-06-19 17:25:12 +0300

    product tracking automation

---
+++ MVCWEF/Models/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCWEF.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+        public int Count { get; set; }
+    }
+}
